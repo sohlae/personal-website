63b51aa87166816e5644f73d728f45b28f70acb0
Masoud Soleymani <msoolymani@gmail.com>
2026-04-10 00:00:30 +0100

    fix: redirect links

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,5 +1,5 @@
 export const navLinks = [
-  { href: '/#about', label: 'About' },
-  { href: '/#experience', label: 'Experience' },
-  { href: '/#projects', label: 'Projects' },
+  { href: '#about', label: 'About' },
+  { href: '#experience', label: 'Experience' },
+  { href: '#projects', label: 'Projects' },
 ] as const;
